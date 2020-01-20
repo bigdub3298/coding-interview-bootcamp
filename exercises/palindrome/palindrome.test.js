605f0b33a51dd4126a4eb7cab70a965652458920
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-20 00:19:55 -0700

    implemented test suite for isPalindrome

---
+++ exercises/palindrome/palindrome.test.js
@@ -0,0 +1,29 @@
+const isPalindrome = require("./palindrome");
+
+test("isPalindrome exists", () => {
+  expect(isPalindrome).toBeDefined();
+});
+
+test("isPalndrome of empty string", () => {
+  expect(isPalindrome("")).toBeFalsy();
+});
+
+test("isPalindrome level", () => {
+  expect(isPalindrome("level")).toBeTruthy();
+});
+
+test("isPalindrome with special characters !asa", () => {
+  expect(isPalindrome("!asa")).toBeFalsy();
+});
+
+test("isPalindrome acccca", () => {
+  expect(isPalindrome("acccca")).toBeTruthy();
+});
+
+test("isPalindrome whack", () => {
+  expect(isPalindrome("whack")).toBeFalsy();
+});
+
+test("isPalindrome Level (with capitals)", () => {
+  expect(isPalindrome("Level")).toBeTruthy();
+});
