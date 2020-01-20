18fdb6382ae3c2a9568d47e9b7069aa2cf516bb6
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-19 23:34:36 -0700

    added more tests

--- exercises/reverse-string/reverseString.test.js
+++ exercises/reverse-string/reverseString.test.js
@@ -4,6 +4,18 @@ test("reverseString function exists", () => {
   expect(reverseString).toBeDefined();
 });
 
-test("reverseString reverses hello ", () => {
+test("reverseString with empty string", () => {
+  expect(reverseString("")).toBe("");
+});
+
+test("reverseString with hello ", () => {
   expect(reverseString("hello")).toBe("olleh");
+});
+
+test("reverseString with accde !", () => {
+  expect(reverseString("accde !")).toBe("! edcca");
+});
+
+test("reverseString with WesleY", () => {
+  expect(reverseString("WesleY")).toBe("YelseW");
 });
