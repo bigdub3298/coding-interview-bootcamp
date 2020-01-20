ae4cc29a914e4a362fd61abb185bfa70deb2692d
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-19 23:40:13 -0700

    added second solution for reverse string

--- exercises/reverse-string/reverseString.js
+++ exercises/reverse-string/reverseString.js
@@ -1,9 +1,18 @@
-reverseString = string => {
+// Solution 1
+const reverseString = string => {
   let reverseString = "";
   for (let i = string.length - 1; i > -1; i--) {
     reverseString += string[i];
   }
   return reverseString;
 };
+
+// Solution 2
+// const reverseString = string => {
+//   return string
+//     .split("")
+//     .reverse()
+//     .join("");
+// };
 
 module.exports = reverseString;
