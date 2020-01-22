895f15c931a40b94f3f40a6f008f5e2d39c738c2
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-22 09:14:44 -0700

    implemented 3 addtional solutions for array chunk

--- exercises/arraychunk/chunk.js
+++ exercises/arraychunk/chunk.js
@@ -1,3 +1,4 @@
+// Solution 1
 const chunk = (arr, size) => {
   const chunkedArr = [];
   for (let i = 0; i < arr.length; i += size) {
@@ -5,5 +6,55 @@ const chunk = (arr, size) => {
   }
   return chunkedArr;
 };
+
+// Solution 2
+// const chunk = (arr, size) => {
+//   const chunkedArr = [];
+
+//   let chunk = [];
+//   for (const item of arr) {
+//     chunk.push(item);
+//     if (chunk.length % size === 0) {
+//       chunkedArr.push(chunk);
+//       chunk = [];
+//     }
+//   }
+
+//   if (arr.length % size !== 0) {
+//     chunkedArr.push(chunk);
+//   }
+
+//   return chunkedArr;
+// };
+
+// Solution 3
+// const chunk = (arr, size) => {
+//   const chunkedArr = arr.reduce((accum, curr, index) => {
+//     const chunkIndex = index % size;
+//     if (chunkIndex == 0) {
+//       accum.push([curr]);
+//     } else {
+//       accum[accum.length - 1].push(curr);
+//     }
+//     return accum;
+//   }, []);
+
+//   return chunkedArr;
+// };
+
+// Solution 4
+// const chunk = (arr, size) => {
+//   const chunkedArr = [];
+
+//   for (let item of arr) {
+//     const last = chunkedArr[chunkedArr.length - 1];
+//     if (!last || last.length === size) {
+//       chunkedArr.push([item]);
+//     } else {
+//       last.push(item);
+//     }
+//   }
+//   return chunkedArr;
+// };
 
 module.exports = chunk;
