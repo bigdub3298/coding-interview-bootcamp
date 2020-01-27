2952f3e6709027b3ebc16163b6df0f02018f2893
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-27 14:37:23 -0700

    added solution to matrix

---
+++ exercises/matrix/matrix.js
@@ -0,0 +1,39 @@
+// solution 1
+const matrix = number => {
+  const result = [];
+  for (let i = 0; i < number; i++) {
+    result.push([]);
+  }
+
+  let startRow = 0,
+    endRow = number - 1,
+    startColumn = 0,
+    endColumn = number - 1,
+    counter = 1;
+
+  while (startColumn <= endColumn && startRow <= endRow) {
+    // top row
+    for (let i = startColumn; i <= endColumn; i++) {
+      result[startRow][i] = counter++;
+    }
+    startRow++;
+    // last column
+    for (let i = startRow; i <= endRow; i++) {
+      result[i][endColumn] = counter++;
+    }
+    endColumn--;
+    // bottom row
+    for (let i = endColumn; i >= startColumn; i--) {
+      result[endRow][i] = counter++;
+    }
+    endRow--;
+    // first column
+    for (let i = endRow; i >= startRow; i--) {
+      result[i][startColumn] = counter++;
+    }
+    startColumn++;
+  }
+  return result;
+};
+
+module.exports = matrix;
